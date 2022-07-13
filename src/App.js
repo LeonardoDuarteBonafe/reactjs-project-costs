446b1d8df17cdc8aded684ec935cec933ddf592c
Leonardo Duarte <leo_nardo136@hotmail.com>
2022-07-13 01:02:50 -0300

    feature: creating footer and navbar

--- src/App.js
+++ src/App.js
@@ -3,25 +3,26 @@ import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
+
 import Container from './components/pages/layout/Container';
+import Navbar from './components/pages/layout/Navbar'
+import Footer from './components/pages/layout/Footer'
 
 function App() {
   return (
     <Router>
-
-      <div>
-        <Link to='/'>Home</Link>
-        <Link to='/contact'>Contato</Link>
-        <Link to='/company'>Empresa</Link>
-        <Link to='/newproject'>Novo Projeto</Link>
-      </div>
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route exact path='/company' element={<Company />} />
-        <Route exact path='/contact' element={<Contact />} />
-        <Route exact path='/newproject' element={<NewProject />} />
-      </Routes>
-      <footer>Footer</footer>
+      <Navbar />
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/projects' element={<Projects />} />
+          <Route exact path='/company' element={<Company />} />
+          <Route exact path='/contact' element={<Contact />} />
+          <Route exact path='/newproject' element={<NewProject />} />
+        </Routes>
+      </Container>
+      <Footer />
     </Router>
   );
 }
