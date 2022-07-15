95243db59929e3b8fa43f2871a42a595a5473fd9
Leonardo Duarte <leo_nardo136@hotmail.com>
2022-07-15 19:41:11 -0300

    feature: creating delete project function

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -11,7 +11,8 @@ import { useState, useEffect } from "react";
 
 function Projects() {
   const [projects, setProjects] = useState([]);
-  const [removeLoading, setRemoveLoading] = useState(false);
+    const [removeLoading, setRemoveLoading] = useState(false);
+    const [projectMessage, setProjectMessage] = useState('')
 
   const location = useLocation();
   let message = "";
@@ -37,13 +38,29 @@ function Projects() {
     }, 300);
   }, []);
 
+  function removeProject(id) {
+    fetch(`http://localhost:5000/projects/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then(() => {
+        setProjects(projects.filter((project) => project.id !== id));
+        setProjectMessage('Projeto removido com sucesso')
+      })
+      .catch((err) => console.log(err));
+  }
+
   return (
     <div className={styles.project_container}>
       <div className={styles.title_container}>
         <h1>Meus Projetos</h1>
         <LinkButton to="/newproject" text="Criar Projeto" />
       </div>
-      {message && <Message type="success" msg={message} />}
+          {message && <Message type="success" msg={message} />}
+          {projectMessage && <Message type="success" msg={projectMessage} />}
       <Container customClass="start">
         {projects.length > 0 &&
           projects.map((project) => (
@@ -52,7 +69,8 @@ function Projects() {
               name={project.name}
               budget={project.budget}
               category={project.category.name}
-              key={project.id}
+                  key={project.id}
+                  handleRemove={removeProject}
             />
           ))}
         {!removeLoading && <Loading />}
