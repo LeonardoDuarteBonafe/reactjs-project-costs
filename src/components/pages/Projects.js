1cf158667cdf43c5a1f35c1d7810cadddc1179e8
Leonardo Duarte <leo_nardo136@hotmail.com>
2022-07-15 01:32:43 -0300

    feature: creating loader object

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -2,6 +2,7 @@ import { useLocation } from "react-router-dom";
 
 import Message from "./layout/Message";
 import Container from "./layout/Container";
+import Loading from "./layout/Loading";
 import LinkButton from "./layout/LinkButton";
 import ProjectCard from "./project/ProjectCard";
 
@@ -10,6 +11,7 @@ import { useState, useEffect } from "react";
 
 function Projects() {
   const [projects, setProjects] = useState([]);
+  const [removeLoading, setRemoveLoading] = useState(false);
 
   const location = useLocation();
   let message = "";
@@ -18,18 +20,21 @@ function Projects() {
   }
 
   useEffect(() => {
-    fetch("http://localhost:5000/projects", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((resp) => resp.json())
-      .then((data) => {
-        console.log(data);
-        setProjects(data);
+    setTimeout(() => {
+      fetch("http://localhost:5000/projects", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
       })
-      .catch((err) => console.log(err));
+        .then((resp) => resp.json())
+        .then((data) => {
+          console.log(data);
+          setProjects(data);
+          setRemoveLoading(true);
+        })
+        .catch((err) => console.log(err));
+    }, 300);
   }, []);
 
   return (
@@ -41,13 +46,19 @@ function Projects() {
       {message && <Message type="success" msg={message} />}
       <Container customClass="start">
         {projects.length > 0 &&
-                  projects.map((project) => <ProjectCard
-                      id={project.id}
-                      name={project.name}
-                      budget={project.budget}
-                      category={project.category.name}
-                      key={project.id}
-                  />)}
+          projects.map((project) => (
+            <ProjectCard
+              id={project.id}
+              name={project.name}
+              budget={project.budget}
+              category={project.category.name}
+              key={project.id}
+            />
+          ))}
+        {!removeLoading && <Loading />}
+        {removeLoading && projects.length === 0 && (
+          <p>Não há projetos cadastrados</p>
+        )}
       </Container>
     </div>
   );
